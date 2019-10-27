bced54b208975b6a8fd744948c97fd5ce11bcbc1
mtmonacelli <taylormonacelli@gmail.com>
2019-10-27 15:27:32 -0700

    Convert from base64 mime back to html

--- receive/app.py
+++ receive/app.py
@@ -12,6 +12,22 @@ app = flask.Flask(__name__)
 cors = flask_cors.CORS(app, resources={r"/*": {"origins": "*"}})
 
 
+def mime_to_html(data, mime_path, html_path, html_path_orig):
+    """ convert back from base64 to UTF-8 or ISO-8859-1 """
+
+    text = ""
+    try:
+        text = base64.b64decode(data).decode(encoding="UTF-8")
+
+    except UnicodeDecodeError as ex:
+        app.logger.exception(ex)
+        text = base64.b64decode(data).decode(encoding="ISO-8859-1")
+
+    html_path.write_text(text)
+    html_path_orig.write_text(text)
+    app.logger.debug(f"{html_path.resolve()} written")
+
+
 def generate_filename_stem(base):
     stem = base
 
@@ -41,9 +57,12 @@ def save_and_process():
 
         scratch_dir = pathlib.Path("/tmp/scratch").resolve()
         mime_path = scratch_dir / f"{stem}.mime"
+        html_path_orig = scratch_dir / f"{stem}-orig.html"
+        html_path = scratch_dir / f"{stem}.html"
 
         scratch_dir.mkdir(exist_ok=True)
         mime_path.write_text(data)
+        mime_to_html(data, mime_path, html_path, html_path_orig)
 
     resp = flask.jsonify(success=True)
     return resp
