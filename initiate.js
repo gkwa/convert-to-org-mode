7ea5ccd210f163981dbf12d432fc11d99f62f5cf
mtmonacelli <taylormonacelli@gmail.com>
2019-11-28 12:02:26 -0800

    Working
    Revert "Working"
    
    This reverts commit 7576de86d7877be29a86d314f71944acc3fb76d7.
    
    Attempt 1
    
    cleanup

--- initiate.js
+++ initiate.js
@@ -3,30 +3,26 @@
 // @namespace https://taylorm.net
 // @version   1
 // @match     *://*/*
+// @require   https://craig.is/assets/js/mousetrap/mousetrap.min.js?9d308
+// @require   https://cdn.jsdelivr.net/npm/alertifyjs@1.12.0/build/alertify.min.js
+// @require   file:///Users/mtm/pdev/taylormonacelli/convert-to-org-mode/initiate.js
+// @require   file:///Users/mtm/pdev/taylormonacelli/convert-to-org-mode/library.js
+// @resource  alertifyCSS https://cdnjs.cloudflare.com/ajax/libs/AlertifyJS/1.12.0/css/alertify.min.css
+// @resource  alertifyThemeCSS https://cdnjs.cloudflare.com/ajax/libs/AlertifyJS/1.12.0/css/themes/default.min.css
 // @grant     GM_addStyle
 // @grant     GM_getResourceText
 // @grant     GM_getResourceURL
-// @require   https://craig.is/assets/js/mousetrap/mousetrap.min.js?9d308
-// @require   https://cdn.jsdelivr.net/npm/alertifyjs@1.12.0/build/alertify.min.js
-// @require   file:///Users/mtm/pdev/taylormonacelli/convert-to-org-mode/library.js
-// @require   file:///Users/mtm/pdev/taylormonacelli/convert-to-org-mode/initiate.js
-// @resource  alertifyCSS https://cdnjs.cloudflare.com/ajax/libs/AlertifyJS/1.12.0/css/alertify.min.css
-// @resource  alertifyThemeCSS https://cdnjs.cloudflare.com/ajax/libs/AlertifyJS/1.12.0/css/themes/default.min.css
 // ==/UserScript==
-
-document.head.appendChild(cssElement(GM_getResourceURL("alertifyCSS")));
-document.head.appendChild(cssElement(GM_getResourceURL("alertifyThemeCSS")));
 
 const publish = async (url, options) => {
   return new Promise((resolve, reject) => {
     fetchAsync(url, options)
       .then(data => {
-        console.log(data);
         resolve(data);
       })
       .catch(reason => {
-        console.log(reason.message);
-        reject(reason.message);
+        console.log(reason);
+        reject(reason);
       });
   });
 };
@@ -52,7 +48,7 @@ const sendPageToPort = async () => {
   let options;
 
   encodedString = b64EncodeUnicode(innerHTML);
-  console.log(`data substring:${encodedString.substring(1, 100)}`);
+  console.log(`debug data substring: ${encodedString.substring(1, 100)}`);
 
   options = {
     method: "GET",
@@ -63,14 +59,13 @@ const sendPageToPort = async () => {
   url = `${urlbase}/healthcheck`;
   publish(url, options)
     .then(data => {
-      console.log(data);
       alertify.notify("Endpoint is open", "success", 2, function() {
-        console.log("dismissed");
+        console.log("alertifyjs reporting: dismissed");
       });
     })
     .catch(reason => {
-      alertify.notify(reason, "error", 0, function() {
-        console.log("dismissed");
+      alertify.notify(reason.message, "error", 0, function() {
+        console.log("alertifyjs reporting: dismissed");
       });
     });
 
@@ -90,25 +85,48 @@ const sendPageToPort = async () => {
   };
   url = `${urlbase}`;
   publish(url, options)
-    .catch(reason => {
-      alertify.notify(reason, "error", 0, function() {
-        console.log("dismissed");
-      });
-    })
     .then(data => {
-      console.log(data);
       alertify.notify("Save completed", "success", 0, function() {
-        console.log("dismissed");
+        console.log("alertifyjs reporting: dismissed");
       });
     })
     .catch(reason => {
-      alertify.notify(reason, "error", 0, function() {
-        console.log("dismissed");
+      alertify.notify(reason.message, "error", 0, function() {
+        console.log("alertifyjs reporting: dismissed");
       });
     });
 };
 
 (function() {
   "use strict";
+  // ------------------------------
+  console.log("==> Script start.", new Date());
+
+  // 1ST PART OF SCRIPT RUN GOES HERE.
+  console.log("==> 1st part of script run.", new Date());
+
+  document.addEventListener("DOMContentLoaded", DOM_ContentReady);
+  window.addEventListener("load", pageFullyLoaded);
+
+  function DOM_ContentReady() {
+    // 2ND PART OF SCRIPT RUN GOES HERE.
+    // This is the equivalent of @run-at document-end
+    console.log("==> 2nd part of script run.", new Date());
+
+    document.head.appendChild(cssElement(GM_getResourceURL("alertifyCSS")));
+    document.head.appendChild(
+      cssElement(GM_getResourceURL("alertifyThemeCSS"))
+    );
+  }
+
+  function pageFullyLoaded() {
+    console.log("==> Page is fully loaded, including images.", new Date());
+  }
+
+  console.log("==> Script end.", new Date());
+  // ------------------------------
+
   Mousetrap.bind("ctrl+s", sendPageToPort, "keydown");
+  document.head.appendChild(cssElement(GM_getResourceURL("alertifyCSS")));
+  document.head.appendChild(cssElement(GM_getResourceURL("alertifyThemeCSS")));
 })();
