e88e19f115a206673e43b4697620117a396205be
mtmonacelli <taylormonacelli@gmail.com>
2020-02-13 18:20:10 -0800

    Kludge: get org files from volume

---
+++ get_org_files_from_volume.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -e
+
+docker rm orgstore
+docker run --mount src=myorgfiles,dst=/tmp/scratch --name orgstore ubuntu /bin/bash
+docker run --rm --volumes-from orgstore -v $(pwd):/backup ubuntu tar -cf /backup/backup.tar /tmp/scratch/
+tar --delete -f backup.tar --wildcards tmp/scratch/*{.html,.log,.mime}
+docker run --mount src=myorgfiles,dst=/tmp/scratch ubuntu find /tmp/scratch -type f -name '*.org' -delete
+docker run --mount src=myorgfiles,dst=/tmp/scratch ubuntu find /tmp/scratch -type f -name '*.org' | cat -n -
+tar xf backup.tar
+du -shc tmp/scratch/*.org
