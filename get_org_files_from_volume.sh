0dc43f026e2ee314c7ada90b66895d05c4acab3a
Taylor Monacelli <taylormonacelli@gmail.com>
2022-07-03 18:48:50 -0700

    default tar on macos doesn't support --delete flag, use gnu-tar instead

--- get_org_files_from_volume.sh
+++ get_org_files_from_volume.sh
@@ -8,7 +8,7 @@ set -e
 
 docker run --mount src=myorgfiles,dst=/tmp/scratch --name orgstore ubuntu /bin/bash
 docker run --rm --volumes-from orgstore -v $(pwd):/backup ubuntu tar -cf /backup/backup.tar /tmp/scratch/
-tar --delete -f backup.tar --wildcards tmp/scratch/*{.html,.log,.mime}
+gtar --delete -f backup.tar --wildcards tmp/scratch/*{.html,.log,.mime}
 docker run --mount src=myorgfiles,dst=/tmp/scratch ubuntu find /tmp/scratch -type f -name '*.org' -delete
 docker run --mount src=myorgfiles,dst=/tmp/scratch ubuntu find /tmp/scratch -type f -name '*.org' | cat -n -
 tar xf backup.tar
